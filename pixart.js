78351e0bcaca3f27d6d84e3f4fdca53d183fc98a
hsingfu <hsingfu.t@gmail.com>
2017-10-20 15:41:26 +1100

    3/10 finally, wrote shitty code though

--- pixart.js
+++ pixart.js
@@ -1,32 +1,78 @@
 console.log("pixel");
 
-$colorField = $('#color-field');
-$brush = $('.brush');
-$body = $('body');
-
-
+var $colorField = $('#color-field');
+var $brush = $('.brush');
+var $body = $('body');
 
 $('#set-color').on('click', function(event){
 	event.preventDefault();
-	colorSpec = $colorField.val()
+	var colorSpec = $colorField.val()
 	$brush.prop("style").background = colorSpec;
 	// console.log('color');
-})
-
-var createDivs = function (){
-	for(i=0; i<1000; i++){
-		$sqDiv = $('<div>').prop({ class: "square" });
-		$body.append($sqDiv);
-	}
-};
-
-createDivs();
 
 $body.on("mouseover",function(event){
-	$eventTarget = $(event.target)	
+	var $eventTarget = $(event.target)	
 	if ($eventTarget.prop("class")==="square"){
 		// $eventTarget.addClass("green");
 		$eventTarget.prop("style").background = $colorField.val();
 
 	}
 })
+
+})
+
+var createDivs = function (){
+	for(i=0; i<1000; i++){
+		var $sqDiv = $('<div>').prop({ class: "square" });
+		$body.append($sqDiv);
+	}
+};
+
+createDivs();
+
+
+
+// ----------------OMDB AJAX STAMP TOOL --------------//
+
+var $movieName = $('#movie-name');
+var $poster = $('.poster');
+
+$('#get-poster').on('click', function(event){
+	event.preventDefault();
+
+	var options = {
+		url: "http://omdbapi.com/",
+		method: 'get',
+		data: { apikey: '2f6435d9', t: $movieName.val()}
+	}
+
+	var appendSearchResult = function(resp){
+		image_url = resp["Poster"];
+		console.log(resp["Poster"]);
+		$poster.css('background-image', "url("+image_url+")" );
+
+		
+	}
+
+	$.ajax(options).done(appendSearchResult);
+
+
+	$body.on("mouseover",function(event){
+	var $eventTarget = $(event.target)	
+	if ($eventTarget.prop("class")==="square"){
+		// $eventTarget.addClass("green");
+		$eventTarget.prop("style").background = $poster.prop("style").backgroundImage;
+
+	}
+})
+
+});
+
+
+
+
+
+
+
+
+
